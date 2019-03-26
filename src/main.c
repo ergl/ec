0bcc13b393573fc18aab6d416c7731b0b535bba3
ergl <ergl@users.noreply.github.com>
2019-03-26 10:45:24 +0100

    Bring over main from assignment 4

--- src/main.c
+++ src/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "44b.h"
-#include "button.h"
 #include "leds.h"
 #include "utils.h"
 #include "D8Led.h"
@@ -11,8 +10,7 @@
 
 // Pin macros
 #define KB_PIN 1
-#define BTTN1_PIN 6
-#define BTTN2_PIN 7
+#define BUF_SIZE 4
 
 // Timer helper methods
 enum tmr_seconds {
@@ -25,33 +23,23 @@ enum tmr_seconds {
 // Default timer CMP register value
 #define TMR_CMP 1
 
-struct RLstat {
-    // 0 -> still, 1 -> moving
-    int moving;
-    // speed in loops
-    int speed;
-    // 0 -> counterclockwise, 1 -> clockwise
-    int direction;
-    // position represented as index in
-    // Segments[] (see D8Led.c)
-    // Should be always between 0 and 5
-    // (modulo movement, always in circles)
-    int position;
+// FSM state
+enum state {
+    INIT = 0,
+    SHOW_PASS = 1,
+    GUESS = 2,
+    SHOW_GUESS = 3,
+    GAME_OVER = 4
 };
 
-static struct RLstat RL = {
-    .moving = 1,
-    .speed = 5,
-    .direction = 0,
-    .position = 0,
-};
+// Global game state
+enum state game_state;
 
-// Keep track of button 2 press events
-static unsigned int button2_counter = 0;
+static char password_buf[BUF_SIZE];
+static char guess_buf[BUF_SIZE];
 
 // ISR functions
 void timer_ISR(void) __attribute__ ((interrupt ("IRQ")));
-void button_ISR(void) __attribute__ ((interrupt ("IRQ")));
 void keyboard_ISR(void) __attribute__ ((interrupt ("IRQ")));
 
 // XXX: To update mode and prescaler, do we need to restart?
@@ -140,72 +128,13 @@ int set_timer_to(enum tmr_timer t, enum tmr_seconds seconds, enum tmr_mode mode)
     return setup_timer(t, prescaler_value, div_value, timer_count, mode);
 }
 
+// TODO(borja): Timer 0 ISR logic
 void timer_ISR(void) {
-    // If it's not moving, no need to redraw
-    if (!RL.moving) {
-        goto cleanup_timer_isr;
-    }
-
-    if (RL.direction) {
-        RL.position = modulo(RL.position + 1, 6);
-    } else {
-        RL.position = modulo(RL.position - 1, 6);
-    }
-
-    // Redraw
-    D8Led_segment(RL.position);
-
-cleanup_timer_isr:
     // Need to clear the pending flag
     ic_cleanflag(INT_TIMER0);
 }
 
-void button_ISR(void) {
-    // EXTINTPND is a 4-bit register, telling which lines are active
-    // This ISR is shared by lines 4, 5, 6 and 7
-    unsigned int which_eint_line = rEXTINTPND;
-
-    // Get the two MS bits
-    unsigned int buttons = (which_eint_line >> 2) & 0x3;
-
-    // Push button 1 was pressed
-    if (buttons & BUT1) {
-        // First, turn off both LEDs
-        led1_off();
-        led2_off();
-
-        // Toggle RL direction
-        RL.direction = !RL.direction;
-    }
-
-    if (buttons & BUT2) {
-        // First, increment internal counter, and toggle led accordingly
-        // Might overflow? Swap to a boolean toggle
-        button2_counter++;
-        if (button2_counter & 1) {
-            // Odd, toggle LED 2
-            led2_switch();
-        } else {
-            // Even, toggle LED 1
-            led1_switch();
-        }
-
-        // Then, toggle RL.moving
-        RL.moving = !RL.moving;
-    }
-
-    // Wait for debounce
-    Delay(2000);
-
-    // Clean ISR flags
-
-    // Write back the default value
-    rEXTINTPND = which_eint_line;
-
-    // Now clear the flag on the interrupt controller
-    ic_cleanflag(INT_EINT4567);
-}
-
+// TODO(borja): Implement keyboard ISR
 void keyboard_ISR(void) {
     int key;
     enum digital key_state = LOW;
@@ -216,29 +145,6 @@ void keyboard_ISR(void) {
     key = kb_scan();
     if (key == -1) {
         goto exit_kb_isr;
-    }
-
-    D8Led_digit(key);
-
-    switch (key) {
-        case 0:
-            // Set timer 0: freq 2s, presc 255, count 62500, div 1/8
-            setup_timer(TIMER0, 255, D1_8, 62500, RELOAD);
-            break;
-        case 1:
-            // Set timer 0: freq 1s, presc 255, count 31250, div 1/8
-            setup_timer(TIMER0, 255, D1_8, 31250, RELOAD);
-            break;
-        case 2:
-            // Set timer 0: freq 0.5s, presc 255, count 15625, div 1/8
-            setup_timer(TIMER0, 255, D1_8, 15625, RELOAD);
-            break;
-        case 3:
-            // Set timer 0: freq 0.25s, presc 255, count 15625, div 1/4
-            setup_timer(TIMER0, 255, D1_4, 15625, RELOAD);
-            break;
-        default:
-            break;
     }
 
     // Wait until key is depressed
@@ -259,28 +165,12 @@ exit_kb_isr:
 }
 
 int setup(void) {
-    // Initialize LEDs
-    leds_init();
-
-    // Initialize and light up appropriate segment
+    // Initialize 8-segment display
     D8Led_init();
-    D8Led_segment(RL.position);
 
     // ------------------------------------------------------------
     // Port G configuration
     // ------------------------------------------------------------
-
-    // Enable button handling via interrupts (pins 6 and 7)
-    // * Enable pin mode to EINT
-    // * Set up pull-up registry
-    // * Set up interrupt mode for that pin
-    portG_conf(BTTN1_PIN, EINT);
-    portG_conf_pup(BTTN1_PIN, ENABLE);
-    portG_eint_trig(BTTN1_PIN, FALLING);
-
-    portG_conf(BTTN2_PIN, EINT);
-    portG_conf_pup(BTTN2_PIN, ENABLE);
-    portG_eint_trig(BTTN2_PIN, FALLING);
 
     // Enable keyboard handling via interrupts (pin 1 on port G)
     // Pull-up is not really needed for keyboard, but we might as well
@@ -291,12 +181,7 @@ int setup(void) {
     // ------------------------------------------------------------
     // Timer 0 configuration
     // ------------------------------------------------------------
-
-    // Set up timer 0 to fire every 2s
     set_timer_to(TIMER0, ONE_SEC, RELOAD);
-    if (RL.moving) {
-        tmr_start(TIMER0);
-    }
 
     // ------------------------------------------------------------
     // Set up ISR handlers
@@ -306,10 +191,6 @@ int setup(void) {
     pISR_EINT1 = (int) keyboard_ISR;
     pISR_TIMER0 = (int) timer_ISR;
 
-    // Register ISR for buttons (shared ISR for lines 4, 5, 6 and 7)
-    // Will need to determine if buttons were pressed inside button_ISR
-    // This is done reading the EXTINTPND[3:0] register
-    pISR_EINT4567 = (int) button_ISR;
 
     // ------------------------------------------------------------
     // Configure interrupt controller
@@ -324,12 +205,10 @@ int setup(void) {
 
     // Configure timer, push buttons keyboard lines in IRQ
     ic_conf_line(INT_TIMER0, IRQ);
-    ic_conf_line(INT_EINT4567, IRQ); // Buttons
     ic_conf_line(INT_EINT1, IRQ); // Keyboard
 
     // Enable timer, push buttons and keyboard lines
     ic_enable(INT_TIMER0);
-    ic_enable(INT_EINT4567);
     ic_enable(INT_EINT1);
 
     // Finally, unmask the global register
@@ -347,6 +226,40 @@ int setup(void) {
 }
 
 int loop(void) {
+    switch (game_state) {
+        case INIT:
+            // TODO(borja) Implement
+            // D8 = C
+            // Fill the buffer while key != F
+            // Check that buffer size >= 4, otherwise error
+            // FIXME(borja): if (OK) SHOW_PASS else INIT
+            game_state = SHOW_PASS;
+            break;
+        case SHOW_PASS:
+            // TODO(borja): Implement
+            // Every second, show pass on D8
+            game_state = GUESS;
+            break;
+        case GUESS:
+            // TODO(borja): Implement
+            // D8 = F
+            // Fill the buffer while key != F
+            // Check that buffer size >= 4, otherwise error
+            // FIXME(borja): if (OK) SHOW_GUESS else GUESS
+            game_state = SHOW_GUESS;
+            break;
+        case SHOW_GUESS:
+            // TODO(borja): Implement
+            // Every second, show guess on D8
+            game_state = GAME_OVER;
+            break;
+        case GAME_OVER:
+            // TODO(borja): Check guess == pass
+            // FIXME(borja): if (OK) INIT else GUESS
+            game_state = INIT;
+            break;
+    }
+
     return 0;
 }
 
